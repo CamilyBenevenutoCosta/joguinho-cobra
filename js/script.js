0445c42ef72973a02a1a5181696125b5a016de6d
Cami_Costa <camily.bc.veleiros@gmail.com>
2025-05-13 15:18:00 -0300

    13/05/2025

--- js/script.js
+++ js/script.js
@@ -81,7 +81,7 @@ controls.forEach(button => button.addEventListener("click", () => changeDirectio
 // Começar o Game = init Game
 const initGame = () => {
     if (gameOver) return handleGameOver();
-    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"`;
+    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
 
     // Quando a cobra come
     if (snakeX === foodX && snakeY === foodY) {
@@ -107,4 +107,20 @@ const initGame = () => {
     if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
         return gameOver = true;
     }
+
+    // Add div para cada parte do corpo da cobra
+    for (let i = 0; i < snakeBody .length; i++) {
+        html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+
+        // Verifica se a cabeça da cobra atingiu ou colidiu com o corpo
+        if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+            gameOver = true;
+        }
+        playBoard.innerHTML = html;
+    }
+
 }
+
+updateFoodPosition();
+setIntervalId = setInterval(initGame, 100);
+document.addEventListener("keyup", changeDirection);
